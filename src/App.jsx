bac49dec6a6bd3050c8fd274563430f96fdb4c4e
Aditya Bhalerao <jerrybhalerao17@gmail.com>
2023-12-21 18:08:21 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './components/header';
+import Header from './components/Header';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
