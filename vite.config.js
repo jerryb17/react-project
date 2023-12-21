9c933d23d3b9392d3cfb71077331602d974a62d6
Aditya Bhalerao <jerrybhalerao17@gmail.com>
2023-12-21 18:21:49 +0530

    Your commit message here

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 10000, // Adjust this value based on your preference (e.g., 1000 KB)
+  },
   plugins: [react()],
-})
+});
